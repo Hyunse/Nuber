a132e2032a57e13c43509b0f887265e212dc567f
KimHyunse <anzmf12@naver.com>
2018-09-13 22:52:40 -0400

    Add Twilio, StartPhoneVerification

---
+++ server/src/api/User/StartPhoneVerification/StartPhoneVerification.resolvers.ts
@@ -0,0 +1,53 @@
+import { Resolvers } from '../../../types/resolvers';
+import {
+  StartPhoneVerificationMutationArgs,
+  StartPhoneVerificationResponse
+} from '../../../types/graph';
+import Verification from '../../../entities/Verification';
+import { sendVerificationSMS } from '../../../utils/sendSMS';
+
+const resolvers: Resolvers = {
+  Mutation: {
+    StartPhoneVerification: async (
+      _,
+      args: StartPhoneVerificationMutationArgs
+    ): Promise<StartPhoneVerificationResponse> => {
+      // PhoneNumber is a number that we get from client
+      const { phoneNumber } = args;
+      
+      try {
+        // Find PhoneNumber 
+        const existingVerfication = await Verification.findOne({
+          payload: phoneNumber
+        });
+
+        if (existingVerfication) {
+          existingVerfication.remove();
+        }
+
+        const newVerification = await Verification.create({
+          payload: phoneNumber,
+          target: 'PHONE'
+        }).save();
+
+        console.log(newVerification);
+        // Send SMS
+        await sendVerificationSMS(newVerification.payload, newVerification.key);
+
+        // Return
+        return {
+          ok: true,
+          error: null
+        };
+      } catch (error) {
+        console.log(error);
+        return {
+          ok: false,
+          error: error.message
+        };
+      }
+    }
+  }
+};
+
+export default resolvers;
