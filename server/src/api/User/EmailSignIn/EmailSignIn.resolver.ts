0af3c0a750cc113f48609ea252b0fe6e6ae23e94
KimHyunse <anzmf12@naver.com>
2018-09-13 20:05:44 -0400

    Email SignIn

---
+++ server/src/api/User/EmailSignIn/EmailSignIn.resolver.ts
@@ -0,0 +1,53 @@
+import { Resolvers } from '../../../types/resolvers';
+import {
+  EmailSignInMutationArgs,
+  EmailSignInResponse
+} from '../../../types/graph';
+import User from '../../../entities/User';
+
+const resolvers: Resolvers = {
+  Mutation: {
+    EmailSignIn: async (
+      _,
+      args: EmailSignInMutationArgs
+    ): Promise<EmailSignInResponse> => {
+      const { email, password } = args;
+
+      try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+          return {
+            ok: false,
+            error: 'No User with that email',
+            token: 'Comming soon'
+          };
+        }
+
+        const checkPassword = await user.comparePassword(password);
+
+        if(checkPassword) {
+          return {
+            ok: true,
+            error: null,
+            token: 'Comming soon'
+          }
+        } else {
+          return {
+            ok: false,
+            error: 'Worong password',
+            token: null
+          }
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null
+        };
+      }
+    }
+  }
+};
+
+export default resolvers;
