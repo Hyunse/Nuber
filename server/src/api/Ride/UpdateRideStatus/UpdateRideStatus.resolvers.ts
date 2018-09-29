c30a54e95842ef903636a2d4015350741f092443
KimHyunse <anzmf12@naver.com>
2018-09-28 21:55:04 -0400

    GetRide, UpdateRideStatus

---
+++ server/src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
@@ -0,0 +1,73 @@
+import { Resolvers } from '../../../types/resolvers';
+import privateResolver from '../../../utils/privateResolver';
+import User from '../../../entities/User';
+import {
+  UpdateRideStatusMutationArgs,
+  UpdateRideStatusResponse
+} from '../../../types/graph';
+import Ride from '../../../entities/Ride';
+
+const resolvers: Resolvers = {
+  Mutation: {
+    UpdateRideStatus: privateResolver(
+      async (
+        _,
+        args: UpdateRideStatusMutationArgs,
+        { req }
+      ): Promise<UpdateRideStatusResponse> => {
+        const user: User = req.user;
+
+        if (user.isDriving) {
+          try {
+            let ride: Ride | undefined;
+            if (args.status === 'ACCEPTED') {
+              // Driver Accept
+              ride = await Ride.findOne({
+                id: args.rideId,
+                status: 'REQUESTING'
+              });
+
+              if (ride) {
+                ride.driver = user;
+                user.isTaken=  true;
+                user.save();
+              }
+            } else {
+              ride = await Ride.findOne({
+                id: args.rideId,
+                driver: user
+              });
+            }
+
+            if (ride) {
+              ride.status = args.status;
+              ride.save();
+
+              return {
+                ok: true,
+                error: null
+              };
+            } else {
+              return {
+                ok: false,
+                error: `Can't update ride`
+              };
+            }
+          } catch (error) {
+            return {
+              ok: false,
+              error: error.message
+            };
+          }
+        } else {
+          return {
+            ok: false,
+            error: `You're not a driver`
+          }
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
