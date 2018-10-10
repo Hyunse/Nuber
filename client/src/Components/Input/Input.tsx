4e894df7d6a0fc5efbe84905bf94067afd93cfd9
KimHyunse <anzmf12@naver.com>
2018-10-09 21:03:46 -0400

    PhoneLogin IProps IState

--- client/src/Components/Input/Input.tsx
+++ client/src/Components/Input/Input.tsx
@@ -4,7 +4,7 @@ import styled from '../../typed-components';
 // S - Styled Component
 const Container = styled.input`
   border: none;
-  border-bottom: 2px solid ${props => props.theme.greyColor};
+  border-bottom: 2px solid ${(props) => props.theme.greyColor};
   font-size: 20px;
   width: 100%;
   padding-bottom: 10px;
@@ -13,6 +13,20 @@ const Container = styled.input`
 `;
 // E - Styled Component
 
-const Input = ({ placeholder }) => <Container placeholder={placeholder} />
+interface IProps {
+  placeholder?: string;
+  type: string;
+  required: boolean;
+  value: any;
+}
+
+const Input: React.SFC<IProps> = ({ placeholder = '', type = 'text', required = true, value }) => (
+  <Container
+    type={type}
+    required={required}
+    value={value}
+    placeholder={placeholder}
+  />
+);
 
 export default Input;
